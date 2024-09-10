647a382a79a4f946a97d06240f1fd2679110b984
frlopez88 <68876977+frlopez88@users.noreply.github.com>
2024-09-09 19:13:41 -0600

    My First Commit
    Some data

---
+++ script.js
@@ -0,0 +1,46 @@
+// Mini Database of shopping Kart
+
+let kart = [
+   {
+    id: 1, 
+    name : "Playstation 5", 
+    price: 1000, 
+    quantity: 1
+   }, 
+   {
+    id: 2, 
+    name: "Make Up", 
+    price: 5, 
+    quantity: 3
+   } , 
+   {
+    id: 3, 
+    name: "T Shirts", 
+    price: 10, 
+    quantity: 10
+   }
+]
+// Render all the elements into a table
+
+function renderAllElements(){
+
+   let tableLayout = ` <tr> 
+                            <th>Name</th>
+                            <th>Price</th>
+                            <th>Quantity</th>
+                            <th>Total</th>
+                        </tr> `
+
+    for( let i=0; i <kart.length; i++){
+        tableLayout += ` <tr> 
+                            <td> ${kart[i].name} </td>
+                            <td>$${kart[i].price}</td>
+                            <td>${kart[i].quantity}</td>
+                            <td>$${kart[i].price * kart[i].quantity}</td>
+                        </tr> `
+    }
+
+    allElements.innerHTML = tableLayout
+
+
+}
